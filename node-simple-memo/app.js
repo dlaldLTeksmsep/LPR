68ae57b113d4ea89f939ba942b6e716a42ca0856
jeongdain <dain9528@naver.com>
2021-03-26 11:05:01 +0900

    UPDATE

--- node-simple-memo/app.js
+++ node-simple-memo/app.js
@@ -3,15 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var check = require('./routes/check')
 var indexRouter = require('./routes/index');
+
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,7 +18,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/check',check);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- node-simple-memo/routes/index.js
+++ node-simple-memo/routes/index.js
@@ -1,69 +1,127 @@
+// var express = require('express');
+// var router = express.Router();
+// const db = require('../db');
+// const {check, validationResult} = require('express-validator');
+
+// /* GET home page. */
+// router.get('/', function (req, res, next) {
+//   db.getAllMemos((rows) => {
+//     res.render('index', { rows: rows });
+//   });
+// });
+
+// router.get('/newMemo', function (req, res, next) {
+//   res.render('newMemo');
+// });
+
+// router.get('/checks', function (req, res, next) {
+//   res.render('checks');
+// });
+
+// router.get('/updateMemo', (req, res) => {
+//   let id = req.query.id;
+//   db.getMemoById(id, (row) => {
+//     if (typeof id === 'undefined' || row.length <= 0) {
+//       res.status(404).json({ error: 'undefined memo' });
+//     }
+//     else { res.render('updateMemo', { row: row[0] }); }
+//   });
+// });
+
+// router.post('/store', [check('content').isByteLength({ min: 1, max: 500 })], function (req, res, next) {
+//   let errs = validationResult(req);
+//   if (errs['errors'].length > 0) {
+//     res.render('newMemo', { errs: errs['errors'] });
+//   } else {
+//     let param = JSON.parse(JSON.stringify(req.body));
+//     db.insertMemo(param['content'], () => {
+//       res.redirect('/');
+//     });
+//   }
+// });
+
+// router.get('/deleteMemo', (req, res) => {
+//   let id = req.query.id; db.deleteMemoById(id, () => { res.redirect('/'); });
+// });
+
+
+
+
+// module.exports = router;
+
+
 var express = require('express');
 var router = express.Router();
-const { check, validationResult } = require('express-validator');
+const {check, validationResult} = require('express-validator');
+
 const db = require('../db');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  db.getAllMemos((rows) => {
+router.get('/', function(req, res, next) {
+
+  db.getAllMemos((rows) =>{
     res.render('index', { rows: rows });
   });
 });
 
-router.get('/newMemo', function (req, res, next) {
+router.get('/checks', function (req, res, next) {
+  res.render('checks');
+});
+
+//page move
+router.get('/newMemo',  function(req, res, next){
   res.render('newMemo');
 });
 
-
-router.post('/store', [check('content').isByteLength({ min: 1, max: 500 })], function (req, res, next) {
+router.post('/store', [check('content').isByteLength({min:1, max:500})], function(req, res, next){
   let errs = validationResult(req);
-  if (errs['errors'].length > 0) {
-    res.render('newMemo', { errs: errs['errors'] });
-  } else {
+  console.log(errs);
+  if(errs['errors'].length > 0){ //화면에 에러 출력하기 위함
+    res.render('newMemo',{errs:errs['errors']});
+  }else{
     let param = JSON.parse(JSON.stringify(req.body));
-    db.insertMemo(param['content'], () => {
+    db.insertMemo(param['content'],() =>{
       res.redirect('/');
     });
   }
 });
 
-router.get('/check',function(req,res){
-  console.log('test');
-  res.render('check', { errs: errs['errors'] }); 
-})
+router.get('/updateMemo', (req, res) =>{
+    let id = req.query.id;
 
-router.get('/updateMemo', (req, res) => {
+    db.getMemoById(id, (row)=>{
+      if(typeof id === 'undefined' || row.length <= 0){
+        res.status(404).json({error:'undefined memo'});
+      }else{
+        res.render('updateMemo',{row:row[0]});
+      }
+    });
+});
+
+router.post('/updateMemo', [check('content').isLength({min:1, max:500})], (req, res) =>{
+  let errs = validationResult(req);
+
+  let param = JSON.parse(JSON.stringify(req.body));
+  let id = param['id'];
+  let content = param['content'];
+
+  if(errs['errors'].length > 0){ //화면에 에러 출력하기 위함
+
+    db.getMemoById(id, (row)=>{ //유효성 검사에 적합하지 않으면 정보를 다시 조회 후, updateMemo 페이지를 다시 랜더링한다.
+      res.render('updateMemo',{row:row[0], errs:errs['errors']});
+    });
+  }else{
+    db.updateMemoById(id, content, () =>{
+      res.redirect('/');
+    });
+  }
+});
+
+router.get('/deleteMemo', (req, res) =>{
   let id = req.query.id;
-  db.getMemoById(id, (row) => {
-    if (typeof id === 'undefined' || row.length <= 0) {
-      res.status(404).json({ error: 'undefined memo' });
-    }
-    else { res.render('updateMemo', { row: row[0] }); }
+  db.deleteMemoById(id, () =>{
+    res.redirect('/');
   });
 });
 
-router.post('/updateMemo', [check('content').isLength({ min: 1, max: 500 })], (req, res) => {
-  let errs = validationResult(req);
-  let param = JSON.parse(JSON.stringify(req.body));
-  let id = param['id']; let content = param['content'];
-  if (errs['errors'].length > 0) { //화면에 에러 출력하기 위함 
-    db.getMemoById(id, (row) => { //유효성 검사에 적합하지 않으면 정보를 다시 조회 후, updateMemo 페이지를 다시 랜더링한다. 
-      res.render('updateMemo', { row: row[0], errs: errs['errors'] });
-    });
-  } else {
-    db.updateMemoById(id, content, () => {
-      res.redirect('/');
-    });
-  }
-});
-
-router.get('/deleteMemo', (req, res) => {
-  let id = req.query.id; db.deleteMemoById(id, () => { res.redirect('/'); });
-});
-
-
-
-
 module.exports = router;
-
-
